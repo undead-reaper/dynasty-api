e7d2ad88ff919b3ed1e0d3956273c6dc4aff5efc
Dharam Soni <dharamsoni1010@gmail.com>
2026-01-31 12:52:42 +0530

    Initial App Commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.status(200).json({ message: "Welcome to the Dynasty API" });
+});
+
+app.get("/ping", (_, res) => {
+  res.status(200).json({ message: "pong" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
